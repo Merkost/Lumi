67f43bcf57618be78a0a25c34782cfa2cc50469c
merkost <konstantinlikes@gmail.com>
2024-08-17 19:51:58 +1000

    Created Lumi theme

--- app/src/main/java/com/merkost/lumi/ui/theme/Color.kt
+++ app/src/main/java/com/merkost/lumi/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.merkost.lumi.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DeepRed = Color(0xFFB71C1C)
+val BrightYellow = Color(0xFFFFEB3B)
+val GoldenYellow = Color(0xFFFFF176)
+val DarkOrange = Color(0xFFF57C00)
+val DarkerOrange = Color(0xffc86300)
+
+val Brown = Color(0xFF8B4513)
+val Red = Color(0xFFF44336)
+val Orange = Color(0xFFFFA500)
+val Green = Color(0xFF4CAF50)
+
--- app/src/main/java/com/merkost/lumi/ui/theme/Theme.kt
+++ app/src/main/java/com/merkost/lumi/ui/theme/Theme.kt
@@ -1,26 +1,37 @@
 package com.merkost.lumi.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DeepRed,
+    secondary = BrightYellow,
+    tertiary = GoldenYellow,
+    background = Color.White,
+    surface = DarkOrange,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F)
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = DeepRed,
+    secondary = BrightYellow,
+    tertiary = GoldenYellow,
+    background = Color.Black,
+    surface = DarkerOrange,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.White,
+    onBackground = Color(0xFFE0E0E0),
+    onSurface = Color(0xFFE0E0E0)
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -36,19 +47,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun LumiTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     MaterialTheme(
         colorScheme = colorScheme,
